74701939413f01acbb0564ba11833dd6b486162f
wowyomad <vadimsun8837@gmail.com>
2022-04-23 01:42:03 +0300

    Первый пошел

--- Tree1.cpp
+++ Tree1.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+struct Tree
+{
+	int data;
+	Tree* left;
+	Tree* right;
+};
+
+void TreeConstruct(Tree* root,const int data);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Tree* root = NULL;
+	TreeConstruct(root, 0);
+}
+
+void TreeConstruct(Tree* root, const int data)
+{
+	root = new Tree{ data, NULL, NULL };
 }
